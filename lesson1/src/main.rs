072496d95efd17c25ada92230d5f14e294bdb265
phil <phil@bitbrain.co.za>
2023-10-12 07:53:01 +0200

    init

---
+++ lesson1/src/main.rs
@@ -0,0 +1,32 @@
+use rand::random as randomizer;
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    fn new() -> Self {
+        Self {
+            x: randomizer(),
+            y: randomizer(),
+        }
+    }
+
+    fn add(self, other: Point) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+fn main() {
+    let point = Point::new();
+    let other = Point::new();
+
+    let third_point = point.add(other);
+
+    println!("Point: {}, {}", point.x, point.y);
+    println!("New Point: {}, {}", third_point.x, third_point.y);
+}
